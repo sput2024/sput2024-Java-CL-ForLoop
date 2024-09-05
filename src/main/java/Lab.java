ed8313b7a92a0eddc1e837e5b1960e6696a0a6b3
Saumya Puthenveettil <sputhen2000@gmail.com>
2024-09-05 21:53:37 +0000

    Completed Coding Lab: For Loop

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -43,7 +43,9 @@ public class Lab {
     public String returnNumbers(int start, int end){
         String buildingString = "";
 
-//        code here
+       for (int i=start;i<end;i++){
+                   buildingString = buildingString + i;
+                 }
 
         return buildingString;
     }
